a8731ecd229dcaf5a6d7204065c0fb8f85b02836
Luca Grassitelli <lucagrassitelli@gmail.com>
2023-01-06 18:50:08 +0100

    final

--- run_analysis.R
+++ run_analysis.R
@@ -41,6 +41,6 @@ col_mean_std$activities <- factor(col_mean_std$activities, levels=activities_nam
 new_df <- col_mean_std %>% group_by(subjects,activities) %>% summarise_all(mean)
 new_df <- as.data.frame(new_df)
 write.table(new_df, file = "final.txt", row.name=FALSE)
-
+new_df
 
 
